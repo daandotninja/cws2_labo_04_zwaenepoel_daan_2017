707de5a14189f203821da05c5f33f266815d5fd5
Daan  Zwaenepoel <daan@Daans-MBP.lan>
2017-03-23 22:52:23 +0100

    done

---
+++ public/config.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Config - Database Config
+ * ----------------------------------------------------------------
+ */
+
+	//Database Server Host
+	define ('DB_HOST', 'localhost');
+
+	// Database Server Username
+	define ('DB_USER', 'root');
+
+	// Database Server Password
+	define ('DB_PASS', '');
+
+	// Database Name
+	define ('DB_NAME', 'vintageshop');
+
+// EOF
---
+++ public/furniture.php
@@ -0,0 +1,53 @@
+<?php 
+	
+	// /**
+	//  * Includes
+	//  * ----------------------------------------------------------------
+	//  */
+
+		// config & functions
+		require_once '../vendor/twig/twig/lib/Twig/Autoloader.php';
+		Twig_Autoloader::register();
+		$loader = new Twig_Loader_Filesystem( 'templates');
+		$twig = new Twig_Environment($loader);
+		require_once 'config.php';
+		require_once 'functions.php';
+
+		$db = getDatabase();
+
+
+        $stmt1 = $db->prepare('SELECT * FROM items WHERE category=?');
+		$stmt1->execute(array($_GET['category']));
+		$items = $stmt1->fetchAll(PDO::FETCH_ASSOC);
+
+
+
+		// $stmt2 = $db->prepare('SELECT * FROM occasies WHERE type= ? ');
+		// $stmt2->execute(array("houtblazer"));
+		// $houtblazers = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+
+  
+  //       $stmt3 = $db->prepare('SELECT * FROM occasies WHERE type=?');
+  //       $stmt3->execute(array("koperblazer"));
+  //       $koperblazers = $stmt3->fetchAll(PDO::FETCH_ASSOC);
+
+
+
+
+	// /**
+	// * No action to handle: show our page itself
+	// * ----------------------------------------------------------------
+	// */
+
+		// Get all todo items from databases
+	
+		//$what = $items['what'];
+		$url = $_SERVER['PHP_SELF'];
+		$tpl = $twig->loadTemplate('object.twig');
+
+		echo $tpl->render(array(
+			'items' => $items
+
+		));
+
+ ?>
